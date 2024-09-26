015e7495f7b707d014a2f4edf777cb795fc79e36
larissagmz <larissagimenes.tinfem2023@gmail.com>
2024-09-25 22:50:51 -0300

    vsfd

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function meuMetodoMap(array, callback) {
     }
     return arrayNew;
 }
+
 function meuMetodoFilter(array, callback) {
     const arrayNew = [];
     for (let i = 0; i < array.length; i++) {
@@ -15,12 +16,18 @@ function meuMetodoFilter(array, callback) {
     }
     return arrayNew;
 }
-function meuMetodoReduce(array, callback) {
+let array2 = ["oi", "lari", "gusta", "milho", "gusta"];
+function meuMetodoReduce(array, callback, valorInicial) {
     let num = 0;
+    if (valorInicial) {
+        num = valorInicial;
+    }
     for (let i = 0; i < array.length; i++) {
         num = callback(num, array[i], i, array);
     }
+    return num;
 }
+
 function meuMetodoFind(array, callback) {
     let num = 0;
     for (let i = 0; i < array.length; i++) {
@@ -30,23 +37,52 @@ function meuMetodoFind(array, callback) {
         }
     }
 }
-let array2 = ["oi", "lari", "gusta", "milho", "gusta"];
-function meuMetodoIncludes(array, callback) {
-    let arrayNew = [];
-    for (let i = 0; i < array.length; i++) {
-        if (callback(array[i], i)) {
-            arrayNew.push(array[i]);
+
+function meuMetodoIncludes(array, searchElement, fromIndex) {
+    let num = 0;
+    if (fromIndex) {
+        num = fromIndex;
+        if (fromIndex < 0) {
+            let reversed = array.reverse();
+            console.log(reversed);
+
+            for (let i = 0; i <= Math.abs(num); i++) {
+                if (reversed[i] === searchElement) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
-    return arrayNew;
+    for (let i = num; i < array.length; i++) {
+        if (array[i] === searchElement) {
+            return true;
+        }
+    }
+    return false;
 }
 
-function meuMetodoIndesOf(array, callback) {
-    let arrayNew = [];
-    for (let i = 0; i < array.length; i++) {
-        if (callback(array[i], i)) {
-            arrayNew.push(i);
+function meuMetodoIndexOf(array, searchElement, fromIndex) {
+    let indice = -1;
+    if (fromIndex) {
+        indice = fromIndex;
+
+        if (indice < 0) {
+            for (let i = Math.abs(indice); i >= 0; i--) {
+                if (array[i] === searchElement) {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
-    return arrayNew;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === searchElement) {
+            return i;
+        }
+    }
+    return -1;
 }
+
+console.log(meuMetodoIndexOf(array, 8, -6));
